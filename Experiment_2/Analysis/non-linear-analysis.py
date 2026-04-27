d701c19b685bc17bec562a203dffac4fe477e161
Ezra Guerrero <ezga923@gmail.com>
2026-04-26 21:12:14 -0400

    update experiment 2

--- Experiment_2/Analysis/non-linear-analysis.py
+++ Experiment_2/Analysis/non-linear-analysis.py
@@ -3,7 +3,8 @@ import pandas as pd
 import numpy as np
 from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
-from scipy.odr import ODR, Model, Data, RealData
+# from scipy.odr import ODR, Model, Data, RealData
+from scipy.stats import chisquare
 
 # Global Variables
 
@@ -177,84 +178,54 @@ def repeated_values_to_mean_with_err(x:np.ndarray, y:np.ndarray) -> tuple[np.nda
     
     return np.array(out_x), np.array(out_y), np.array(out_y_err)
 
-# Run code
-
-if __name__ == "__main__":
-    calibration = get_data(DATA_DIR + "\\calibration-4_02.csv")
-    freqs = measurements_to_mean_with_err(calibration["f"], 3)
-    gain2s = measurements_to_mean_with_err(calibration["gain^2"], 3)
-    data = get_data(DATA_DIR + "\\data-4_02-fix.csv")
-    
-    g2, g2err = gain2s
-    f, ferr = freqs
-    f = f * 1000
-    ferr = ferr * 1000
+def find_G(R, C, f, ferr, g2, g2err):
     y = []
-    yerr2 = []
-    Cs = np.array([44.06, 45.72, 46.97, 46.80, 48.33])
-    Cval = 74.390
-    Cerr = 0.329
-    C = Cval * 10**-12
-    Cerr = Cerr * 10**-12
-
-    print(C*10**12, Cerr*10**12)
-
-    plot_y_vs_x(*freqs, *gain2s, output_dir=PLOT_DIR, filename="temp-calibration.png", xlabel="Frequency [kHz]", ylabel="Gain^2")
-
     for i in range(len(f)):
-        row_y = []
-        row_err = []
-        row_y.append(g2[i] / (1 + (2 * np.pi * f[i] * C * R) ** 2))
+        y.append(g2[i] / (1 + (2 * np.pi * f[i] * C * R) ** 2))
+    y = np.asarray(y)
+    yerr = y * 0 # CHANGE TO TRUTH IF NECESSARY
 
-        err2 = (g2err[i] / (1 + (2 * np.pi * f[i] * C * R) ** 2)) ** 2 # g2 err contribution
+    return numerical_integral(f, ferr, y, yerr)
 
-        err2 += (g2[i] / (1 + (2 * np.pi * f[i] * C * R) ** 2) ** 2 * 2 * f[i] * (2 * np.pi * C * R) ** 2 * ferr[i]) ** 2 # f err contribution
-        err2 += (g2[i] / (1 + (2 * np.pi * f[i] * C * R) ** 2) ** 2 * 2 * C * (2 * np.pi * f[i] * R) ** 2 * Cerr) ** 2 # C err contribution
-        err2 += (g2[i] / (1 + (2 * np.pi * f[i] * C * R) ** 2) ** 2 * 2 * C * (2 * np.pi * f[i] * C) ** 2 * Rerr) ** 2 # R err contribution
-        row_err.append(err2)
-        y.append(row_y)
-        yerr2.append(row_err)
-    y = np.array(y)
-    yerr2 = np.array(yerr2)
-    yerr = np.sqrt(yerr2)
+def make_fit_func(f, ferr, g2, g2err):
+    def fit_func(R, k, C):
+        G = find_G(R, C, f, ferr, g2, g2err)[0]
+        return 4 * ROOM_TEMPERATURE * R * k * G
+    return fit_func
 
-    Gval, Gerr = numerical_integral(f, ferr, y, yerr)
+if __name__ == '__main__':
 
-    print(Gval)
+    calibration = get_data(DATA_DIR + "\\calibration-3_31.csv")
+    f, g2, g2err = repeated_values_to_mean_with_err(calibration['f'], calibration['gain^2'])
+    f = f * 1000
+    ferr = f * 0.00001
 
-    temp, V2, V2err = repeated_values_to_mean_with_err(data["T"], data["V^2"])
-    V2, V2err = V2*10**-6, V2err*10**-6
-    temperr = np.ones(len(temp)) * .1 / np.sqrt(12)
+    data = get_data(DATA_DIR + "\\data-3_31-fix.csv")
+    Rs = measurements_to_mean_with_err(data['R'], 5)
+    Rs = Rs[0] * 1000, Rs[1] * 1000 + Rs[0] * 10
+    v2s = measurements_to_mean_with_err(data['V^2'], 5)
+    v2s = v2s[0] * (10**-6), v2s[1] * (10**-6)
 
-    quant = V2 / (4 * (99.3 * 1000) * Gval)
-    quant_err = quant * np.sqrt((Gerr/Gval)**2 + (V2err/V2)**2 + (Rerr/R)**2)
+    fit_func = make_fit_func(f, ferr, g2, g2err)
 
-    popt, pcov = curve_fit(weighted_linear, temp, quant, sigma=quant_err/quant)
+    popt, pcov = curve_fit(fit_func, Rs[0], v2s[0], [BOLTZMANN, 79*(10**-12)], sigma=v2s[1], absolute_sigma=True)
+    kfit, Cfit = popt
     perr = np.sqrt(np.diag(pcov))
+    kerr, Cerr = perr
 
-    # odr = ODR(RealData(temp, quant, temperr, quant_err), Model(weighted_linear_odr), [BOLTZMANN, ABSOLUTE_ZERO])
-    # odr.set_job(fit_type=2)
-    # output = odr.run()
-    # outerr = np.sqrt(np.diag(output.cov_beta))
+    xvals = np.linspace(min(Rs[0]), max(Rs[0]), 300)
+    yvals = fit_func(xvals, kfit, Cfit)
 
-    fig, ax = plot_y_vs_x(temp, temperr, quant, quant_err, output_dir=PLOT_DIR, filename="main-plot.png", xlabel="Temperature [C]", ylabel=r"$V^2/4RG$ [J]", close=False)
+    # chi2 = chisquare(v2s, fit_func(Rs[0], *popt), ddof=2)
+    # print(chi2.statistic, chi2.pvalue)
 
-    xvals = np.linspace(-280, max(temp), 500)
-    yvals = weighted_linear(xvals, *popt)
-    # yvalsodr = weighted_linear_odr(output.beta, xvals)
-    yexpvals = weighted_linear(xvals, BOLTZMANN, ABSOLUTE_ZERO)
-
-    ax.plot(xvals, yvals, label="linear regression")
-    ax.plot(xvals, yexpvals, label="prediction")
-    # ax.plot(xvals, yvalsodr, label="odr")
-    ax.vlines(-273.15, min(yvals), max(yexpvals))
-    ax.hlines(0, min(xvals), max(xvals))
+    fig, ax = plot_y_vs_x(*Rs, *v2s, xlabel=r'Resistance [$\Omega$]', ylabel=r'$V^2$ [V$^2$]', output_dir=PLOT_DIR, filename="", close=False)
+    ax.plot(xvals, yvals, label='fit')
 
     ax.legend()
-
+    
     plt.tight_layout()
-    plt.savefig(os.path.join(PLOT_DIR, "main-plot.png"), dpi=300)
+    plt.savefig(os.path.join(PLOT_DIR, "non-linear.png"), dpi=300)
     plt.close('all')
 
-    print(f"k = {popt[0]*10**23:.3f}±{perr[0]*10**23:.3f}, T0 = {popt[1]:.1f}±{perr[1]:.1f}")
-    # print(f"k = {output.beta[0]*10**23:.3f}±{outerr[0]*10**23:.3f}, T0 = {output.beta[1]:.1f}±{outerr[1]:.1f}")
+    print(f'k = {kfit*10**23:.3f}±{kerr*10**23:.3f}, C = {Cfit*10**12:.3f}±{Cerr*10**12:.3f}')
